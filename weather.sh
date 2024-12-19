5862cdff8d3c138fc20c25db2177101c10ef7766
Hugo Camacho Romero <157968467+hugcamrom@users.noreply.github.com>
2024-12-19 22:46:28 +0000

    json no jason

--- weather.sh
+++ weather.sh
@@ -2,6 +2,6 @@
 
 date
 echo "downloading weather data"
-wget -O data/weather/`date +"%Y%m%d_%H%M%S_athenry.jason"` https://prodapi.metweb.ie/observations/athenry/today
+wget -O data/weather/`date +"%Y%m%d_%H%M%S_athenry.json"` https://prodapi.metweb.ie/observations/athenry/today
 echo "weather data downloaded"
 date
